bbee1e1ff7868974278ac1e897af199d185d2b6c
puneeth019 <puneeth019@gmail.com>
2016-07-29 23:58:54 +0530

    Update cleaning_using_dplyr.R

--- cleaning_using_dplyr.R
+++ cleaning_using_dplyr.R
@@ -1,4 +1,4 @@
-#This script is used for cleaning data
+#This script is used to clean data  using `dplyr` package
 
 library(dplyr)  # load `dplyr` package
 options(stringsAsFactors = FALSE) #set global option to read files
