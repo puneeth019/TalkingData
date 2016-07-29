21b9245bebbf04545e0e8438fcb61d41a2824b6f
puneeth019 <puneeth019@gmail.com>
2016-07-30 01:11:04 +0530

    Update read-data_readr.R

--- read-data_readr.R
+++ read-data_readr.R
@@ -5,7 +5,7 @@ setwd("/TalkingData/") # set working directory
 
 app_events <- read_csv("app_events.csv/app_events.csv", col_types = "icii")
 app_labels <- read_csv("app_labels.csv/app_labels.csv", col_types = "ci")
-events <- read_csv("events.csv/events.csv", col_types = "cccii")
+events <- read_csv("events.csv/events.csv", col_types = "iccdd")
 gender_age_train <- read_csv("gender_age_train.csv/gender_age_train.csv", col_types = "ccic")
 label_categories <- read_csv("label_categories.csv/label_categories.csv", col_types = "ic")
 phone_brand_device_model <- read_csv("phone_brand_device_model.csv/phone_brand_device_model.csv", col_types = "ccc")
