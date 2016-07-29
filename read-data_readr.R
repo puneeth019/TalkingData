a1a0e5724caa51705f2d6abea76d9f651d997465
puneeth019 <puneeth019@gmail.com>
2016-07-30 00:32:21 +0530

    Update and rename readr_data_cleaning.R to read-data_readr.R

---
+++ read-data_readr.R
@@ -0,0 +1,24 @@
+#This script is used to read data  using `dplyr` package
+
+library(dplyr)  # load `dplyr` package
+options(stringsAsFactors = FALSE) #set global option to read files
+setwd("/TalkingData/") # set working directory
+
+app_events <- read.csv("app_events.csv/app_events.csv", colClasses = c("integer", "character", "integer", "integer"))
+app_labels <- read.csv("app_labels.csv/app_labels.csv", colClasses = c("character", "integer"))
+events <- read.csv("events.csv/events.csv", colClasses = c("integer", "character", "character", "integer", "integer"))
+gender_age_test <- read.csv("gender_age_test.csv/gender_age_test.csv", colClasses = c("character"))
+gender_age_train <- read.csv("gender_age_train.csv/gender_age_train.csv",
+  colClasses = c("integer", "character", "integer", "character"))
+label_categories <- read.csv("label_categories.csv/label_categories.csv", colClasses = c("integer", "character"))
+phone_brand_device_model <- read.csv("phone_brand_device_model.csv/phone_brand_device_model.csv",
+  colClasses = c("character","character","character"))
+
+#convert "data.frame" into "tibble"
+app_events  <- tbl_df(app_events)
+app_labels <- tbl_df(app_labels)
+events <- tbl_df(events)
+gender_age_test <- tbl_df(gender_age_test)
+gender_age_train <- tbl_df(gender_age_train)
+label_categories <- tbl_df(label_categories)
+phone_brand_device_model <- tbl_df(phone_brand_device_model)
